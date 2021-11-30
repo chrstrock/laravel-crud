7e322514fe90fb05fffb902bb4fcc234451bb816
David Ferrell <chrstrock@gmail.com>
2021-11-30 08:29:39 -0500

    change register route to list page.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const LIST = '/list';
 
     /**
      * The controller namespace for the application.
