ea5ef6846f2741eb0882fd8e3c73d4da4973a645
David Ferrell <chrstrock@gmail.com>
2021-11-30 09:02:31 -0500

    changing route back to list.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-    public const LOGIN = '/login';
+    public const LIST = '/list';
+    public const LOGIN = '/';
 
     /**
      * The controller namespace for the application.
